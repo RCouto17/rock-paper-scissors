0d35567aae207d99b7e3347d21346373e3aa63c9
RCouto17 <rafaelcoutoalves17@gmail.com>
2023-12-19 12:18:47 -0300

    clean code a little and add winner count also add styling to winner

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let pcWin = 0;
 let playerChoice = '';
 let pcChoice= '';
 const container = document.querySelector('.container');
+const pcScore = document.querySelector('.pc-score');
+const playerScore = document.querySelector('.player-score');
+const divPC = document.createElement('div');
+const divResult = document.createElement('div');
+const scoreContainer = document.querySelector('.score-container');
+const winner = document.createElement('div');
 
 function getComputerChoice() {
     let numChoice = Math.random();
@@ -21,109 +27,122 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'ROCK'){
         if (computerSelection == 'ROCK'){
-            const divPC = document.createElement('div');
-            divPC.textContent = 'PC chose rock'; 
+            divPC.textContent = 'PC chose 🪨'; 
             container.appendChild(divPC)
-            const divResult = document.createElement('div');
             divResult.textContent = 'Draw';
             container.appendChild(divResult);
         }
         else if (computerSelection == 'PAPER'){
-            const divPC = document.createElement('div');
-            divPC.textContent = 'PC chose paper'; 
+            divPC.textContent = 'PC chose 📜'; 
             container.appendChild(divPC);
-            const divResult = document.createElement('div');
             divResult.textContent = 'PC wins';
             container.appendChild(divResult);
             pcWin++;
+            pcScore.textContent = pcWin;
         }
         else if (computerSelection == 'SCISSORS'){
-            const divPC = document.createElement('div');
-            divPC.textContent = 'PC chose scissors'; 
+            divPC.textContent = 'PC chose ✂️'; 
             container.appendChild(divPC);
-            const divResult = document.createElement('div');
             divResult.textContent = 'Player wins';
             container.appendChild(divResult);
             playerWin++;
+            playerScore.textContent = playerWin;
         }
     }
     else if (playerSelection == 'PAPER'){
         if (computerSelection == 'PAPER'){
-            const divPC = document.createElement('div');
-            divPC.textContent = 'PC chose paper'; 
+            divPC.textContent = 'PC chose 📜'; 
             container.appendChild(divPC);
-            const divResult = document.createElement('div');
             divResult.textContent = 'Draw';
             container.appendChild(divResult);
         }
         else if (computerSelection == 'SCISSORS'){
-            const divPC = document.createElement('div');
-            divPC.textContent = 'PC chose scissors'; 
+            divPC.textContent = 'PC chose ✂️'; 
             container.appendChild(divPC);
-            const divResult = document.createElement('div');
             divResult.textContent = 'PC wins';
             container.appendChild(divResult);
             pcWin++;
-            
+            pcScore.textContent = pcWin;
         }
         else if (computerSelection == 'ROCK') {
-            const divPC = document.createElement('div');
-            divPC.textContent = 'PC chose rock'; 
+            divPC.textContent = 'PC chose 🪨'; 
             container.appendChild(divPC)
-            const divResult = document.createElement('div');
             divResult.textContent = 'Player wins';
             container.appendChild(divResult);
             playerWin++;
+            playerScore.textContent = playerWin;
         }
     }
     else if (playerSelection == 'SCISSORS'){
         if (computerSelection == 'SCISSORS'){
-            const divPC = document.createElement('div');
-            divPC.textContent = 'PC chose scissors'; 
+            divPC.textContent = 'PC chose ✂️'; 
             container.appendChild(divPC);
-            const divResult = document.createElement('div');
             divResult.textContent = 'Draw';
             container.appendChild(divResult);
         }
         else if (computerSelection == 'ROCK'){
-            const divPC = document.createElement('div');
-            divPC.textContent = 'PC chose rock'; 
+            divPC.textContent = 'PC chose 🪨'; 
             container.appendChild(divPC)
-            const divResult = document.createElement('div');
             divResult.textContent = 'PC wins';
             container.appendChild(divResult);
             pcWin++;
+            pcScore.textContent = pcWin;
         }
         else if (computerSelection == 'PAPER'){
-            const divPC = document.createElement('div');
-            divPC.textContent = 'PC chose paper'; 
+            divPC.textContent = 'PC chose 📜'; 
             container.appendChild(divPC);
-            const divResult = document.createElement('div');
             divResult.textContent = 'Player wins';
             container.appendChild(divResult);
             playerWin++;
+            playerScore.textContent = playerWin;
         }
     }
     else{
         console.log('You have not chosen rock, paper or scissors, press F5 to try again.');
     }
+
+
+    if (playerWin == 5) {
+        winner.textContent = 'Player won';
+        winner.classList.add('winner');  
+
+        if (!scoreContainer.contains(winner)) {
+            scoreContainer.appendChild(winner);
+        }
+
+        playerWin = 0;
+        playerScore.textContent = playerWin;
+        pcWin = 0;
+        pcScore.textContent = pcWin;
+    } else if (pcWin == 5) {
+        winner.textContent = 'PC won';
+        winner.classList.add('winner'); 
+
+        if (!scoreContainer.contains(winner)) {
+            scoreContainer.appendChild(winner);
+        }
+
+        playerWin = 0;
+        playerScore.textContent = playerWin;
+        pcWin = 0;
+        pcScore.textContent = pcWin;
+    }
+    console.log(playerWin);
+    console.log(pcWin);
   }
 
 
 const btnRock = document.querySelector('.btn-rock');
 btnRock.addEventListener('click', () => {playerChoice = 'ROCK'
     playRound(playerChoice, getComputerChoice());
-    console.log(pcChoice);
 });
 
 const btnPaper = document.querySelector('.btn-paper');
 btnPaper.addEventListener('click', () => {playerChoice = 'PAPER'
     playRound(playerChoice, getComputerChoice());
-    console.log(pcChoice);
 });
 
 const btnScissors = document.querySelector('.btn-scissors');
 btnScissors.addEventListener('click', () => {playerChoice = 'SCISSORS'
     playRound(playerChoice, getComputerChoice());
-    console.log(pcChoice);
 });
